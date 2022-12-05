ef0cb8197d0df754619fbb15ac8f6d29cdb4a048
Benjamin Mlynek <benjaminmlynek4@gmail.com>
2022-12-05 13:44:19 +0100

    Added Terraform Cloud as backend for deploying infrastructure.

--- .infrastructure/main.tf
+++ .infrastructure/main.tf
@@ -5,16 +5,23 @@ terraform {
       version = "~> 4.0"
     }
   }
+
+  backend "remote" {
+    organization = "bm-sandbox"
+    workspaces {
+      name = "hello-world"
+    }
+  }
 }
 
-provider "aws" {
-    access_key = "${var.AWS_ACCESS_KEY_ID}"
-    secret_key = "${var.AWS_SECRET_ACCESS_KEY}"
-    token = "${var.AWS_SESSION_TOKEN}"
-    region = "${var.region}"
-}
+/* provider "aws" {
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
+  token      = var.AWS_SESSION_TOKEN
+  region     = var.region
+} */
 
 module "s3" {
-  source = "./s3"
+  source      = "./s3"
   bucket_name = "js-hello-world"
 }
